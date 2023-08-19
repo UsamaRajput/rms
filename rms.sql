27377f98e11a1ab943876017957bc08cb4d3a472
unknown <rajputhusama@gmail.com>
2023-08-18 23:21:18 -0700

    use site bacjend completed dabase updated

--- rms.sql
+++ rms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 08, 2023 at 11:20 PM
+-- Generation Time: Aug 19, 2023 at 08:19 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -61,7 +61,7 @@ CREATE TABLE `mess` (
 --
 
 INSERT INTO `mess` (`id`, `dish1`, `dish2`, `dish1_units`, `dish2_units`, `day`) VALUES
-(1, 'alo indaa', 'chana rice', 12, 16, 1),
+(1, 'alo indaa1', 'chana rice7', 121, 166, 1),
 (2, 'dal frijhj', 'sabziyt', 167, 108, 2),
 (3, 'das', 'cxz', 12, 22, 3),
 (4, 'dsad', 'zxc', 12, 14, 4),
@@ -161,11 +161,18 @@ CREATE TABLE `room_user` (
 
 CREATE TABLE `staff` (
   `id` int(11) NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL DEFAULT 'images/default_user.png',
   `name` varchar(255) NOT NULL,
   `role` varchar(50) NOT NULL,
   `is_active` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `image`, `name`, `role`, `is_active`) VALUES
+(26, 'images/staff/1692338606_17c23deffcaa4cd7909debe54bb5703e.jpg', 'tgdf', 'fggf', 1);
 
 -- --------------------------------------------------------
 
@@ -202,11 +209,35 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `blood_group`, `dob`, `gender`, `email`, `cnic`, `password`, `room_id`, `requested_room`, `dept_id`, `image`, `phone`, `address`, `city`, `is_active`, `is_admin`, `is_verified`, `registered_date`, `room_assigned_date`) VALUES
-(2, 'usama', 'rasheed', 'B+', '2023-07-28', 'male', 'usama1@gmail.com', '2323', '', 11, 11, 1, 'images/users/1690726121_17c23deffcaa4cd7909debe54bb5703e.jpg', 'password', 'password', 'password', 1, 1, 1, '2023-07-29', NULL),
+(2, 'usama', 'rasheed', 'B+', '2023-07-28', 'male', 'usama1@gmail.com', '2323', '', 11, 0, 1, 'images/users/1690726121_17c23deffcaa4cd7909debe54bb5703e.jpg', 'password', 'password', 'password', 1, 1, 1, '2023-07-29', NULL),
 (3, 'usama12', 'rasheed12', 'AB+', '2023-07-30', 'male', 'usama22@gmail.com', '499042', '', 11, 0, 1, 'images/users/1690726090_FB_IMG_1613192819562.jpg', 'password12', 'password12', 'password12', 1, 1, 1, '2023-07-29', NULL),
-(6, 'zc', 'zcx', 'a', '2023-08-01', 'male', 'sad@da.ca', '32423432432', 'ads', 11, 0, 1, 'default_img.png', 'a', 'adsasd', '', 1, 0, 1, '2023-08-05', '2023-08-01'),
-(7, 'sdsd', 'zcxsd', 'a', '2023-08-01', 'male', 'sasd@da.ca', '32423432432', 'ads', 10, 0, 2, 'default_img.png', 'a', 'adsasd', '', 1, 0, 1, '2023-08-05', '2023-08-01'),
-(8, 'usama12s', 'rasheed12s', 'AB+', '2023-07-30', 'male', 'usama22s@gmail.com', '499042', '', 10, 0, 5, 'images/users/1690726090_FB_IMG_1613192819562.jpg', 'password12', 'password12', 'password12', 1, 1, 1, '2023-07-29', NULL);
+(6, 'zc', 'zcx', 'a', '2023-08-01', 'male', 'sad@da.ca', '32423432432', '', 10, 0, 1, 'images/users/1692304102_IMG_20210307_091457~2.jpg', 'a', 'adsasd', '', 1, 0, 1, '2023-08-05', '2023-08-01'),
+(7, 'sdsd', 'zcxsd', 'a', '2023-08-01', 'male', 'sasd@da.ca1', '324234324312', '', 10, 0, 2, 'images/users/1692304199_B612_20210527_185133_695.jpg', 'a', 'adsasd', '', 1, 0, 1, '2023-08-05', '2023-08-01'),
+(8, 'usama12s', 'rasheed12s', 'AB+', '2023-07-30', 'male', 'usama22s@gmail.com', '499042', '$2y$10$2kxdMWMC9OL27J9OQiqCh.9oapNxBohpNyTWk087ui9b.BqIYfNX.', 11, 0, 5, 'images/users/1690726090_FB_IMG_1613192819562.jpg', 'password12', 'password12', 'password12', 1, 0, 1, '2023-07-29', NULL),
+(9, 'usama', 'rasheed', 'A-', '0000-00-00', '', 'usamaR@gmail.com', 'usamaR@gmail.com', '$2y$10$2kxdMWMC9OL27J9OQiqCh.9oapNxBohpNyTWk087ui9b.BqIYfNX.', 0, 0, 1, 'images/users/1691609581_20210316_174343.jpg', '', '', '', 1, 1, 1, '2023-08-09', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `webcontent`
+--
+
+CREATE TABLE `webcontent` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `sup_image` varchar(255) NOT NULL,
+  `sup_name` varchar(255) NOT NULL,
+  `sup_message` text NOT NULL,
+  `main_image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `webcontent`
+--
+
+INSERT INTO `webcontent` (`id`, `title`, `description`, `sup_image`, `sup_name`, `sup_message`, `main_image`) VALUES
+(1, 'ad1', 'sd1', 'images/web/1692221575_FB_IMG_1612424777555.jpg', 'sa1', 'sa1', 'images/web/1692221575_4ee9eb5298a34c52b4672cebf6385c7d.jpg');
 
 --
 -- Indexes for dumped tables
@@ -261,6 +292,12 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `webcontent`
+--
+ALTER TABLE `webcontent`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -304,13 +341,19 @@ ALTER TABLE `room_user`
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `webcontent`
+--
+ALTER TABLE `webcontent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
